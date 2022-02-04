c83a62d09eaa70e781aa8f2e07bb4e94d3ad7a23
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-02-04 16:42:23 -0300

    🧼  lint: add style guide

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Title = styled.h1`
   color: red;
   font-size: 50px;
-`
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <Title>My page</Title>;
 }

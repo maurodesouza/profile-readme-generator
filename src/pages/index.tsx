7d42bf3ee6077a46bd7a737d34ca993c2129accb
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-02-05 01:11:34 -0300

    ⚡  chore: testing initial structure
    Testing adding and removing content from canvas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,33 @@
-import styled from 'styled-components';
+import { Canvas } from 'components';
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`;
+import { useCanvas } from 'hooks';
 
-export default function Home() {
-  return <Title>My page</Title>;
-}
+import { events } from '@events/base';
+import { Blocks, Events } from 'types';
+
+const Home = () => {
+  const { contents } = useCanvas();
+
+  const add = () => {
+    events.emit(Events.CANVAS_ADD_CONTENT, {
+      type: Blocks.TEXT,
+      content: 'Ahhh... Hellow?',
+    });
+  };
+
+  const remove = () => {
+    const last = contents.length - 1;
+
+    events.emit(Events.CANVAS_REMOVE_CONTENT, contents[last]?.id || '');
+  };
+
+  return (
+    <>
+      <button onClick={add}>Add</button>
+      <button onClick={remove}>Remove</button>
+      <Canvas />
+    </>
+  );
+};
+
+export default Home;

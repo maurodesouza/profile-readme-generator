52270cf7f83513e4d54fb630c07a7681887bfb2b
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-02-07 05:59:16 -0300

    ⚡  chore: add initial styles to canvas

--- src/components/canvas/styles.ts
+++ src/components/canvas/styles.ts
@@ -1,3 +1,12 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  ${({ theme }) => css`
+    align-self: start;
+    padding: ${theme.spacings.xlarge};
+    border-radius: ${theme.border.radius};
+    border-width: ${theme.border.width};
+    border-color: ${theme.colors.border};
+    border-style: solid;
+  `}
+`;

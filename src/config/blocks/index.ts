6d3ac989edb88e56b023ef5a4ba8034929279a4e
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-02-10 03:58:05 -0300

    ⚡  chore: add default blocks props

---
+++ src/config/blocks/index.ts
@@ -0,0 +1,40 @@
+import { Blocks } from 'types';
+
+const defaultBlocksProps = {
+  [Blocks.STATS]: {
+    props: {
+      content: {
+        graphs: {
+          stats: {
+            hide_title: false,
+            hide_rank: false,
+            show_icons: true,
+            include_all_commits: true,
+            count_private: true,
+            disable_animations: false,
+            theme: 'dracula',
+            locale: 'pt-br',
+            hide_border: false,
+          },
+
+          languages: {
+            locale: 'pt-br',
+            hide_title: false,
+            layout: 'compact',
+            theme: 'dracula',
+            hide_border: false,
+          },
+        },
+
+        styles: {
+          align: 'center',
+          direction: 'row',
+        },
+
+        from: 'maurodesouza',
+      },
+    },
+  },
+};
+
+export { defaultBlocksProps };

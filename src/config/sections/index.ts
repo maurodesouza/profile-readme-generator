735b595c537af3e497eae152a11f4917b999b2d8
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-02-11 17:06:24 -0300

    ⚡  chore: add default height to stats section config

--- src/config/sections/index.ts
+++ src/config/sections/index.ts
@@ -6,6 +6,7 @@ const defaultSectionProps = {
       content: {
         graphs: {
           stats: {
+            height: 150,
             hide_title: false,
             hide_rank: false,
             show_icons: true,
@@ -18,6 +19,7 @@ const defaultSectionProps = {
           },
 
           languages: {
+            height: 150,
             locale: 'pt-br',
             hide_title: false,
             layout: 'compact',

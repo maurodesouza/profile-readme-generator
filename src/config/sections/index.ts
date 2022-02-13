c3f6a5c4c3773516db4ee233f1b816d02f9a1963
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-02-13 06:23:24 -0300

    ⚡  chore: separate stats section config in another file

--- src/config/sections/index.ts
+++ src/config/sections/index.ts
@@ -1,46 +1,9 @@
 import { Sections } from 'types';
 
+import { statsSectionConfig } from './stats';
+
 const defaultSectionProps = {
-  [Sections.STATS]: {
-    props: {
-      content: {
-        graphs: {
-          stats: {
-            height: 150,
-            hide_title: false,
-            hide_rank: false,
-            show_icons: true,
-            include_all_commits: true,
-            count_private: true,
-            disable_animations: false,
-            theme: 'dracula',
-            locale: 'pt-br',
-            hide_border: false,
-          },
-
-          languages: {
-            height: 150,
-            locale: 'pt-br',
-            hide_title: false,
-            layout: 'compact',
-            card_width: 320,
-            langs_count: 5,
-            theme: 'dracula',
-            hide_border: false,
-          },
-        },
-
-        styles: {
-          align: 'center',
-          direction: 'row',
-        },
-
-        from: {
-          username: 'maurodesouza',
-        },
-      },
-    },
-  },
+  [Sections.STATS]: statsSectionConfig,
 };
 
 export { defaultSectionProps };

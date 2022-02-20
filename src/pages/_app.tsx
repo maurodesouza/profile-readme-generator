48adb961df5a61374da70818e5871ce69abcecf5
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-02-20 02:40:53 -0300

    ⚡  chore: prevent right click

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,24 @@ import { ThemeProvider } from 'styled-components';
 import { ContextMenu } from 'components';
 import { CanvasProvider } from 'contexts';
 
+import { events } from '@events/index';
 import { theme, GlobalStyles } from 'styles';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const handlePreventRightClick = (e: MouseEvent) => {
+    e.preventDefault();
+
+    events.contextmenu.close();
+  };
+
+  useEffect(() => {
+    events.on('contextmenu', handlePreventRightClick);
+
+    return () => {
+      events.on('contextmenu', handlePreventRightClick);
+    };
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <CanvasProvider>

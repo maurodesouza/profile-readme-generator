efc1c9eaf1850a19219d0304e1064d64c7bd576d
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-02-22 19:17:41 -0300

    ⚡  chore: add textarea styles to input

--- src/components/inputs/input/styles.ts
+++ src/components/inputs/input/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled, { css, DefaultTheme } from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
@@ -13,8 +13,36 @@ export const Label = styled.span`
   `}
 `;
 
-export const Input = styled.input`
-  ${({ theme }) => css`
+type InputProps = {
+  as?: string;
+};
+
+const inputModifiers = {
+  textarea: (theme: DefaultTheme) => css`
+    resize: none;
+    min-height: 10rem;
+    height: 10rem;
+    max-height: 10rem;
+
+    padding-right: ${theme.spacings.small};
+
+    &::-webkit-scrollbar {
+      width: 0.8rem;
+      overflow: hidden;
+    }
+
+    &::-webkit-scrollbar-track {
+      background: transparent;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background: ${theme.colors.border};
+    }
+  `,
+};
+
+export const Input = styled.input<InputProps>`
+  ${({ theme, as = '' }) => css`
     width: 100%;
     background: transparent;
     border-radius: ${theme.border.radius};
@@ -22,5 +50,7 @@ export const Input = styled.input`
     border-color: ${theme.colors.border};
     border-style: solid;
     padding: 8px 12px;
+
+    ${as === 'textarea' && inputModifiers.textarea(theme)};
   `}
 `;

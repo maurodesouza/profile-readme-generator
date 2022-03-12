0905aba9ee0fa22004165e0a52bfef67009dc999
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-12 00:38:43 -0300

    🐛  fix: horizontal scroll

--- src/components/sticky/styles.ts
+++ src/components/sticky/styles.ts
@@ -1,6 +1,8 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  overflow: auto;
+`;
 
 export const Sticky = styled.div`
   ${({ theme }) => css`

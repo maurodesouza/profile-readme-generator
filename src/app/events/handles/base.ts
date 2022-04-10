2f65a6b14c7b0c1ec6d9ea1a2e6eb592f48cdafb
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-04-10 20:33:09 -0300

    ⚡  chore: show event log only in development environment

--- src/app/events/handles/base.ts
+++ src/app/events/handles/base.ts
@@ -1,8 +1,11 @@
+import { config } from 'app';
 import { Events } from 'types';
 
 class BaseEventHandle {
   protected emit(event: Events, payload?: unknown) {
-    console.info(`events[emit]: ${event}`, payload);
+    const isDev = config.envs.environment === 'development';
+
+    isDev && console.info(`events[emit]: ${event}`, payload);
 
     const customEvent = new CustomEvent(event, { detail: payload });
     document.dispatchEvent(customEvent);

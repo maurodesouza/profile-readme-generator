c8a980af95dd74edf102afded0fba1f65af91e01
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-04-10 23:32:24 -0300

    ⚡  chore: improve text section font sizes

--- src/components/sections/text/styles.ts
+++ src/components/sections/text/styles.ts
@@ -5,7 +5,7 @@ export const Container = styled.div`
 `;
 
 type TextProps = {
-  as?: string;
+  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
   align?: 'left' | 'center' | 'right';
 };
 
@@ -16,12 +16,42 @@ const textModifiers = {
     border-bottom-style: solid;
     padding-bottom: ${theme.spacings.xsmall};
   `,
+
+  h1: () => css`
+    font-size: 3.2rem;
+  `,
+
+  h2: () => css`
+    font-size: 2.4rem;
+  `,
+
+  h3: () => css`
+    font-size: 2rem;
+  `,
+
+  h4: () => css`
+    font-size: 1.6rem;
+  `,
+
+  h5: () => css`
+    font-size: 1.4rem;
+  `,
+
+  h6: () => css`
+    font-size: 1.36rem;
+  `,
+
+  p: () => css`
+    font-size: 1.6rem;
+  `,
 };
 
 export const Text = styled.p<TextProps>`
   ${({ theme, as = 'h1', align = 'center' }) => css`
     width: 100%;
     text-align: ${align};
+
+    ${textModifiers[as]};
 
     ${['h1', 'h2'].includes(as) && textModifiers.borderbottom(theme)}
   `}

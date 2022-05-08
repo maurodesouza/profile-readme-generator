e46b3ac5395bc660e077a7db912e48959e663cf4
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-05-08 00:11:59 -0300

    ✨  feat: add workflow generator to snake section

--- src/generators/workflows/sections/index.ts
+++ src/generators/workflows/sections/index.ts
@@ -1,3 +1,13 @@
-const workflowsGeneratorMap = {};
+import { Sections } from 'types';
+
+import { generateSnakeWorkflow } from './snake';
+
+type WorkflowsGeneratoMap = {
+  [key in Sections]?: typeof generateSnakeWorkflow;
+};
+
+const workflowsGeneratorMap: WorkflowsGeneratoMap = {
+  [Sections.SNAKE]: generateSnakeWorkflow,
+};
 
 export { workflowsGeneratorMap };
---
+++ src/generators/workflows/sections/snake.ts
@@ -0,0 +1,42 @@
+const generateSnakeWorkflow = () => {
+  const content = `
+name: Generate snake animation
+
+on:
+  schedule: # execute every 12 hours
+    - cron: "* */12 * * *"
+
+  workflow_dispatch:
+
+  push:
+    branches:
+    - master
+
+  jobs:
+    generate:
+      runs-on: ubuntu-latest
+
+      steps:
+        - name: generate snake.svg
+          uses: Platane/snk@master
+          with:
+            github_user_name: ${'${{ github.repository_owner }}'}
+            outputs: dist/snake.svg
+
+
+        - name: push snake.svg to the output branch
+          uses: crazy-max/ghaction-github-pages@v2.6.0
+          with:
+            target_branch: output
+            build_dir: dist
+          env:
+            GITHUB_TOKEN: ${'${{ secrets.GITHUB_TOKEN }}'}
+`.trim();
+
+  return {
+    file: 'snake.yml',
+    content,
+  };
+};
+
+export { generateSnakeWorkflow };

e059e369691bdfba3869d9e92ad7a41f8d0f1089
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-05-08 00:16:31 -0300

    ⚡  chore: increase result page layout

--- src/templates/result/styles.ts
+++ src/templates/result/styles.ts
@@ -5,8 +5,8 @@ export const Container = styled.div`
     display: grid;
     width: 100%;
     margin: 0 auto;
-    max-width: 118.5rem;
-    grid-template-columns: 20rem 1fr 20rem;
+    max-width: 160rem;
+    grid-template-columns: 26rem 1fr 26rem;
     column-gap: ${theme.spacings.xlarge};
   `}
 `;

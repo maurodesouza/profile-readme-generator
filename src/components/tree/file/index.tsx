68f42bf7fec69425c726c5f422c3f44b894438d8
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-05-08 00:30:47 -0300

    🐛  fix: duplicate name

--- src/components/tree/file/index.tsx
+++ src/components/tree/file/index.tsx
@@ -1,10 +1,10 @@
 import { File as FileIcon } from '@styled-icons/feather';
 import { events } from 'app';
 
-import { File } from '..';
+import { File as FileType } from '..';
 import * as S from './styles';
 
-type FileProps = File;
+type FileProps = FileType;
 
 const File = ({ file, content }: FileProps) => {
   const handleClick = () => events.result.show(content);

077767e842000b5108aaa1c8b82f277f22943daa
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-05-16 15:10:55 -0300

    🐛  fix: empty panel on result page

--- src/templates/result/index.tsx
+++ src/templates/result/index.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
+
+import { events } from 'app';
 import { ReadmeResult, Sticky, Panel } from 'components';
+
+import { PanelsEnum } from 'types';
 import * as S from './styles';
 
 const ResultTemplate = () => {
+  useEffect(() => {
+    events.panel.open('left', PanelsEnum.GENERATED_FILES);
+  }, []);
+
   return (
     <S.Container>
       <Panel side="left" />

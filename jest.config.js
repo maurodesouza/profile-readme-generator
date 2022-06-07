4ed656deafc9133c9d76763c1f310c87a8d54caa
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-06-06 23:44:39 -0300

    ⚡  chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/pages/**/*.ts(x)?',
+    '!src/templates/**/*.ts(x)?',
+    '!src/services/**/*.ts(x)?',
+    '!src/styles/**/*.ts',
+    '!src/types/**/*.ts',
+
+    // Temp
+
+    '!src/app/**/*.ts(x)?',
+    '!src/generators/**/*.ts(x)?',
+    '!src/components/**/*.ts(x)?',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest/'],
+};

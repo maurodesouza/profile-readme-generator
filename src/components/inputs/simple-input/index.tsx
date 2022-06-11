071a4e8d931cd20da1b39459c7f3d1bff7678c82
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-06-11 16:34:01 -0300

    🐛  fix: update input value

--- src/components/inputs/simple-input/index.tsx
+++ src/components/inputs/simple-input/index.tsx
@@ -1,4 +1,10 @@
-import { forwardRef, InputHTMLAttributes } from 'react';
+import {
+  forwardRef,
+  InputHTMLAttributes,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from 'react';
 
 import * as S from './styles';
 
@@ -11,11 +17,19 @@ const SimpleInput: React.ForwardRefRenderFunction<
   HTMLInputElement,
   SimpleInputProps
 > = ({ label, error, className, ...rest }: SimpleInputProps, ref) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useImperativeHandle(ref, () => inputRef.current!);
+
+  useEffect(() => {
+    inputRef.current!.value = (rest.defaultValue as string) || '';
+  }, [rest.defaultValue]);
+
   return (
     <S.Container className={className}>
       {label && <S.Label>{label}</S.Label>}
 
-      <S.Input ref={ref} {...rest} />
+      <S.Input ref={inputRef} {...rest} />
 
       {error && <S.Error>{error}</S.Error>}
     </S.Container>

b95a87b6062eb6659a26ee96a4f870ed27fb0ebf
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-06-14 21:38:58 -0300

    ⚡  chore: add count api config

--- src/app/config/envs/index.ts
+++ src/app/config/envs/index.ts
@@ -1,5 +1,19 @@
 const envs = {
   environment: process.env.NODE_ENV,
+
+  urls: {
+    count_api: process.env.COUNT_API_URL,
+  },
+
+  options: {
+    count_api: {
+      namespace: process.env.COUNT_API_NAMESPACE,
+      keys: {
+        visits: process.env.COUNT_PAGE_VIEW_KEY,
+        'generated-readmes': process.env.COUNT_GENERATED_READMES_KEY,
+      },
+    },
+  },
 };
 
 export { envs };

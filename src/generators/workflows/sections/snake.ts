011942403f09172c7ffdcdbe00f3f53b6aef6134
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-06-15 17:52:54 -0300

    🐛  fix: snake workflow template

--- src/generators/workflows/sections/snake.ts
+++ src/generators/workflows/sections/snake.ts
@@ -12,25 +12,25 @@ on:
     branches:
     - master
 
-  jobs:
-    generate:
-      runs-on: ubuntu-latest
+jobs:
+  generate:
+    runs-on: ubuntu-latest
 
-      steps:
-        - name: generate snake.svg
-          uses: Platane/snk@master
-          with:
-            github_user_name: ${'${{ github.repository_owner }}'}
-            outputs: dist/snake.svg
+    steps:
+      - name: generate snake.svg
+        uses: Platane/snk/svg-only@v2
+        with:
+          github_user_name: ${'${{ github.repository_owner }}'}
+          outputs: dist/snake.svg
 
 
-        - name: push snake.svg to the output branch
-          uses: crazy-max/ghaction-github-pages@v2.6.0
-          with:
-            target_branch: output
-            build_dir: dist
-          env:
-            GITHUB_TOKEN: ${'${{ secrets.GITHUB_TOKEN }}'}
+      - name: push snake.svg to the output branch
+        uses: crazy-max/ghaction-github-pages@v2.6.0
+        with:
+          target_branch: output
+          build_dir: dist
+        env:
+          GITHUB_TOKEN: ${'${{ secrets.GITHUB_TOKEN }}'}
 `.trim();
 
   return {

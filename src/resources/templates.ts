cefc714cc7a73b3c05de3243de7cc12816517838
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-06-17 18:16:15 -0300

    ✨  feat: add new template

--- src/resources/templates.ts
+++ src/resources/templates.ts
@@ -61,6 +61,143 @@ const templates = [
       },
     ],
   },
+
+  {
+    inspired: 'https://github.com/rafaballerini',
+    template: [
+      {
+        type: 'text',
+        props: {
+          content: {
+            text: "Hi 👋! My name is ... and i'm a ..., from ....",
+            as: 'h2',
+          },
+          styles: { align: 'left' },
+        },
+      },
+      {
+        type: 'stats',
+        props: {
+          content: {
+            graphs: {
+              stats: {
+                height: 150,
+                hide_title: false,
+                hide_rank: false,
+                show_icons: true,
+                include_all_commits: true,
+                count_private: true,
+                disable_animations: false,
+                theme: 'dracula',
+                locale: 'en',
+                hide_border: false,
+              },
+              languages: {
+                height: 150,
+                locale: 'en',
+                hide_title: false,
+                layout: 'compact',
+                card_width: 320,
+                langs_count: 5,
+                theme: 'dracula',
+                hide_border: false,
+              },
+            },
+          },
+          styles: { align: 'center' },
+          state: 'default',
+        },
+      },
+      {
+        type: 'image',
+        props: {
+          content: { url: 'https://i.imgflip.com/65efzo.gif' },
+          styles: { align: 'center', height: '150', float: 'right' },
+        },
+      },
+      {
+        type: 'techs',
+        props: {
+          content: {
+            techs: {
+              javascript: { icon: 'original' },
+              typescript: { icon: 'plain' },
+              react: { icon: 'original' },
+              html5: { icon: 'original' },
+              css3: { icon: 'original' },
+              python: { icon: 'original' },
+              csharp: { icon: 'original' },
+            },
+            styles: { height: '30' },
+          },
+          styles: { align: 'left', spacing: 12 },
+        },
+      },
+      {
+        type: 'socials',
+        props: {
+          content: {
+            socials: {
+              youtube: {
+                icon: 'default',
+                message: 'Youtube',
+                color: 'FF0000',
+                label: '',
+                logo: 'youtube',
+                logoColor: 'white',
+              },
+              instagram: {
+                message: 'Instagram',
+                color: 'E4405F',
+                label: '',
+                logo: 'instagram',
+                logoColor: 'white',
+                icon: 'default',
+              },
+              twitch: {
+                message: 'Twitch',
+                color: '9146FF',
+                label: '',
+                logo: 'twitch',
+                logoColor: 'white',
+                icon: 'default',
+              },
+              discord: {
+                icon: 'default',
+                message: 'Discord',
+                color: '7289DA',
+                label: '',
+                logo: 'discord',
+                logoColor: 'white',
+              },
+              gmail: {
+                message: 'Gmail',
+                color: 'D14836',
+                label: '',
+                logo: 'gmail',
+                logoColor: 'white',
+                icon: 'default',
+              },
+              linkedin: {
+                icon: 'default',
+                message: 'LinkedIn',
+                color: '0077B5',
+                label: '',
+                logo: 'linkedin',
+                logoColor: 'white',
+              },
+            },
+            styles: { style: 'for-the-badge', type: 'badge', height: '35' },
+          },
+          styles: { align: 'left', spacing: 12 },
+        },
+      },
+      {
+        type: 'snake',
+        props: { styles: { clear: true }, state: 'default' },
+      },
+    ],
+  },
 ];
 
 export { templates };

e59396621617de13e0c533cd8b357fc20b6d0226
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-06-17 14:37:49 -0300

    🐛  fix: apply clear float on generate readme

--- src/generators/readme/index.ts
+++ src/generators/readme/index.ts
@@ -5,7 +5,9 @@ import { sectionsGeneratorMap } from './sections';
 
 const readmeGenerator = (template: CanvasSection[], settings: Settings) => {
   const readme = template.reduce((readme, section) => {
-    if (section.props.state === CanvasStatesEnum.ALERT) return readme;
+    const { state, styles } = section.props;
+
+    if (state === CanvasStatesEnum.ALERT) return readme;
 
     const generator = (sectionsGeneratorMap as any)[section.type!];
 
@@ -13,7 +15,9 @@ const readmeGenerator = (template: CanvasSection[], settings: Settings) => {
 
     const html = htmlPrettify(generator(section.props, settings));
 
-    return `${readme}\n${html}\n###`;
+    return `${readme}\n${
+      styles.clear ? '\n<br clear="both">\n' : ''
+    }\n${html}\n###`;
   }, '');
 
   const readmeFormated = readme.replace(/(###)/g, '\n$1');

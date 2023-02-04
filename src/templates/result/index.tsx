b8a96f8505080d3174fa192bc46e088af0c20e43
Mauro de Souza <maurodesouza2017@hotmail.com>
2023-02-04 01:27:24 -0300

    ⚡  chore: improve screens usability
    Was removed the building mobile component and also improved some responsive styles

--- src/templates/result/index.tsx
+++ src/templates/result/index.tsx
@@ -1,28 +1,19 @@
-import { useEffect } from 'react';
-
-import { events } from 'app';
-import { ReadmeResult, Panel, BuildingModile } from 'components';
+import { ReadmeResult, Panel } from 'components';
 
 import { PanelsEnum } from 'types';
 import * as S from './styles';
 
 const ResultTemplate = () => {
-  useEffect(() => {
-    events.panel.open('left', PanelsEnum.GENERATED_FILES);
-  }, []);
-
   return (
-    <BuildingModile>
-      <S.Container>
-        <Panel side="left" />
+    <S.Container>
+      <Panel initialPanel={PanelsEnum.GENERATED_FILES} side="left" />
 
-        <S.Wrapper>
-          <ReadmeResult />
-        </S.Wrapper>
+      <S.Wrapper>
+        <ReadmeResult />
+      </S.Wrapper>
 
-        <div />
-      </S.Container>
-    </BuildingModile>
+      <div />
+    </S.Container>
   );
 };
 

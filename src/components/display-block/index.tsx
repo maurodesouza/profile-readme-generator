5f35656dc5fd9cec567f489fb4ea6afd9a8be8eb
Mauro de Souza <maurodesouza2017@hotmail.com>
2023-05-31 00:26:29 -0300

    ⚡  chore: add new props to display block
    added imgWidth and imgHeight

--- src/components/display-block/index.tsx
+++ src/components/display-block/index.tsx
@@ -6,13 +6,26 @@ import * as S from './styles';
 type TechIconProps = HTMLMotionProps<'div'> & {
   label?: string;
   display: string;
+  imgWidth?: string;
+  imgHeight?: string;
 };
 
-const DisplayBlock = ({ label, display, ...rest }: TechIconProps) => {
+const DisplayBlock = ({
+  label,
+  display,
+  imgWidth = '40%',
+  imgHeight = '40%',
+  ...rest
+}: TechIconProps) => {
   return (
     <S.Container variants={variants.container} {...rest}>
       <S.Wrapper>
-        <S.Image alt={`${label} logo`} src={display} />
+        <S.Image
+          alt={`${label} logo`}
+          src={display}
+          imgWidth={imgWidth}
+          imgHeight={imgHeight}
+        />
 
         {label}
       </S.Wrapper>

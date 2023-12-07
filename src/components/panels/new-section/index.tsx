755b2a6c34f9931ae83915c6679b0686cf971dd7
Mauro de Souza <maurodesouza2017@hotmail.com>
2023-12-06 23:21:04 -0300

    ⚡  chore: show the available sections with the extensions

--- src/components/panels/new-section/index.tsx
+++ src/components/panels/new-section/index.tsx
@@ -1,10 +1,28 @@
+import { useExtensions } from 'hooks';
+import { PanelsEnum } from 'types';
+
 import { contents } from './contents';
 import * as S from './styles';
 
 const PanelNewSection = () => {
+  const { extensions } = useExtensions();
+
+  const items = Object.values(
+    extensions[PanelsEnum.NEW_SECTION] ?? {}
+  ) as typeof contents;
+
   return (
     <S.Container>
       {contents.map(({ icon: Icon, name, ...rest }) => (
+        <S.Wrapper key={name} {...rest}>
+          <S.Block>
+            <Icon size={48} />
+            {name}
+          </S.Block>
+        </S.Wrapper>
+      ))}
+
+      {items.map(({ icon: Icon, name, ...rest }) => (
         <S.Wrapper key={name} {...rest}>
           <S.Block>
             <Icon size={48} />

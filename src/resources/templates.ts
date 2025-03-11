aca3dd31ae2d5797ee8d29514c4c46577494e649
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-03-11 20:44:31 -0300

    🐛  fix: image link

--- src/resources/templates.ts
+++ src/resources/templates.ts
@@ -699,7 +699,7 @@ const templates = [
         type: 'image',
         props: {
           content: {
-            url: 'https://camo.githubusercontent.com/62da68eb62b1e5f175f7d1f0191dd89a653d7908feb22d37d4a0ab07365d6791/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f4d3967624264396e6244724f5475314d71782f67697068792e676966',
+            url: 'https://media.giphy.com/media/M9gbBd9nbDrOTu1Mqx/giphy.gif',
           },
           styles: { align: 'center', height: '150', float: 'none' },
         },

ffa321311d52582c46d0a663de036ee01437edf5
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-04-06 10:27:53 -0300

    🐛  fix: images path

--- src/components/panels/recommended-resources/items.ts
+++ src/components/panels/recommended-resources/items.ts
@@ -1,12 +1,12 @@
 const getItems = () => [
   {
-    image: '/assets/clean_code_book.jpg',
+    image: '/assets/books/clean_code_book.jpg',
     name: 'Clean Code',
     description: 'A Handbook of Agile Software Craftsmanship by Uncle Bob.',
     link: 'https://amzn.to/4bR3wGF',
   },
   {
-    image: '/assets/clean_architecture_book.jpg',
+    image: '/assets/books/clean_architecture_book.jpg',
     name: 'Clean Architecture',
     description: 'Software Architecture Solutions from Uncle Bob.',
     link: 'https://amzn.to/41Sg2RG',

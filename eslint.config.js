626e512743c5d630f104b7a8af127705310f5332
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-05-17 22:12:34 -0300

    🧼  lint: warn @typescript-eslint/no-explicit-any

--- eslint.config.js
+++ eslint.config.js
@@ -34,6 +34,7 @@ export default defineConfig([
     rules: {
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
 ]);

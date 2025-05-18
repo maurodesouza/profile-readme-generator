0d3a8be7ae8891a3c97cb9695f912b22cf801420
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-05-17 22:26:46 -0300

    🧼  lint: warn@typescript-eslint/no-unused-expressions and @typescript-eslint/no-unused-vars

--- eslint.config.js
+++ eslint.config.js
@@ -35,6 +35,9 @@ export default defineConfig([
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'warn',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
 ]);

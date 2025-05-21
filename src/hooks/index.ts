e2637aff34cd3d7c5b0f706cfe3e83da5b9b78a3
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-05-21 13:55:18 -0300

    ✨ feat: add use media query hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -5,3 +5,4 @@ export * from './use-persisted-data';
 export * from './use-outside-click';
 export * from './use-extensions';
 export * from './use-callback-ref';
+export * from './use-media-query';

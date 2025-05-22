32c027a26cd5157d19e6f6e399793a603c2ca945
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-05-22 18:11:01 -0300

    ✨ feat: add display-block component

---
+++ src/components/ui/primitives/atoms/display-block.tsx
@@ -0,0 +1,21 @@
+import { twx } from 'utils';
+import { Text } from './text';
+import { Icon as IconPure } from './icon';
+
+const Container = twx.div`group relative w-full rounded-md box-border pt-[100%] transition-colors hover:!border-tone-luminosity-300`;
+
+const Content = twx.div`absolute inset-0 w-full flex flex-col items-center justify-center gap-xs`;
+
+const Icon = twx(
+  IconPure
+)`group-hover:text-tone-foreground-context transition-colors`;
+const Label = twx(
+  Text.Paragraph
+)`group-hover:text-tone-foreground-context transition-colors`;
+
+export const DisplayBlock = {
+  Container,
+  Content,
+  Label,
+  Icon,
+};

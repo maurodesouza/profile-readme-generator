c14cb8e8dd65f432b7b8b838b51ae2e586d60f35
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-05-23 10:51:48 -0300

    ⚡  chore: replace recommended-resources panel with tailwind

--- src/components/panels/recommended-resources/index.tsx
+++ src/components/panels/recommended-resources/index.tsx
@@ -1,37 +1,37 @@
 import { ResourceItem, AffiliateWarning } from 'components';
 
-import * as S from './styles';
+import { Text } from 'components/ui/primitives/atoms/text';
+import { Panel } from 'components/ui/primitives/atoms/panel';
+
 import { getItems } from './items';
 
 const PanelRecommendedResources = () => {
   const items = getItems();
 
   return (
-    <S.Container>
-      <S.Header>
-        <S.Title>Level Up Your README</S.Title>
+    <div className="flex flex-col gap-md h-full">
+      <Text.Heading as="h2">Level Up Your README</Text.Heading>
 
-        <S.Wrapper>
-          <p>
-            Anyone can use templates — but when you know what you’re doing, it
-            shows.
-          </p>
+      <div className="flex flex-col gap-xs">
+        <Text.Paragraph>
+          Anyone can use templates — but when you know what you’re doing, it
+          shows.
+        </Text.Paragraph>
 
-          <p>
-            👉 Explore resources that will upgrade your skills — and your
-            README. Start now.
-          </p>
-        </S.Wrapper>
-      </S.Header>
+        <Text.Paragraph>
+          👉 Explore resources that will upgrade your skills — and your README.
+          Start now.
+        </Text.Paragraph>
+      </div>
 
-      <S.Content>
+      <Panel.Scrollable>
         {items.map(item => (
           <ResourceItem key={item.name} {...item} />
         ))}
-      </S.Content>
+      </Panel.Scrollable>
 
       <AffiliateWarning />
-    </S.Container>
+    </div>
   );
 };
 

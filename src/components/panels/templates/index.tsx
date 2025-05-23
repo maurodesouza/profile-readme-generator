85d49f1dcd4ad6b7b15c22648dbb132e12a77dbf
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-05-23 11:02:04 -0300

    ⚡  chore: replace templates panel with tailwind

--- src/components/panels/templates/index.tsx
+++ src/components/panels/templates/index.tsx
@@ -1,35 +1,45 @@
+import { Text } from 'components/ui/primitives/atoms/text';
+import { Panel } from 'components/ui/primitives/atoms/panel';
+import { DisplayBlock } from 'components/ui/primitives/atoms/display-block';
+
 import { events } from 'app';
 import { templates } from 'resources';
-
 import { CanvasSection } from 'types';
-import * as S from './styles';
 
 const PanelTemplates = () => {
   return (
-    <S.Container>
-      <S.Header>
-        <S.Title>Templates</S.Title>
+    <div className="h-full flex flex-col gap-md">
+      <Text.Heading as="h2">Templates</Text.Heading>
 
-        <S.Text>
-          Select a template to preview, and then confirm to be able to edit.
-        </S.Text>
-      </S.Header>
+      <Text.Paragraph>
+        Select a template to preview, and then confirm to be able to edit.
+      </Text.Paragraph>
 
-      <S.Content>
-        {templates.map(({ template }, index) => (
-          <S.Wrapper
-            key={index}
-            onClick={() => events.template.preview(template as CanvasSection[])}
-          >
-            <S.Block>{index + 1}</S.Block>
-          </S.Wrapper>
-        ))}
-      </S.Content>
+      <Panel.Scrollable>
+        <div className="grid grid-cols-2 gap-md">
+          {templates.map(({ template }, index) => (
+            <button
+              key={index}
+              onClick={() =>
+                events.template.preview(template as CanvasSection[])
+              }
+            >
+              <DisplayBlock.Container>
+                <DisplayBlock.Content>
+                  <DisplayBlock.Label className="text-xl">
+                    {index + 1}
+                  </DisplayBlock.Label>
+                </DisplayBlock.Content>
+              </DisplayBlock.Container>
+            </button>
+          ))}
+        </div>
+      </Panel.Scrollable>
 
-      <S.Small>
+      <Text.Small>
         OBS: on confirm, the template will replace all canvas content.
-      </S.Small>
-    </S.Container>
+      </Text.Small>
+    </div>
   );
 };
 

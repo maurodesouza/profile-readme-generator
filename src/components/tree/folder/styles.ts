b5bacc8a4d426d78a8b2368a7afb55e1e9fb58be
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-05-24 14:13:10 -0300

    ⚡  chore: replace tree component with tailwind

--- src/components/tree/folder/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled, { css } from 'styled-components';
-
-export const Container = styled.div``;
-
-export const Gap = styled.div`
-  ${({ theme }) => css`
-    padding-left: ${theme.spacings.medium};
-  `}
-`;
--- src/components/tree/index.tsx
+++ src/components/tree/index.tsx
@@ -1,25 +1,85 @@
-import { File } from './file';
-import { Folder } from './folder';
+import React from 'react';
+import { IconName } from 'lucide-react/dynamic';
 
-import * as S from './styles';
+import { Icon } from 'components/ui/primitives/atoms/icon';
+import { Text } from 'components/ui/primitives/atoms/text';
 
-export type File = {
+import { cn } from 'utils';
+import { events } from 'app';
+
+export type TFile = {
   file: string;
   content: string;
 };
 
-export type FilesTree = {
-  name: string;
-  files: File[];
+export type TFolder = {
+  name?: string;
+  files: TFile[];
 };
+
+type LabelProps = React.ComponentProps<'div'> & {
+  icon?: IconName;
+};
+
+function Label(props: LabelProps) {
+  const { children, className, icon = 'file', ...rest } = props;
+
+  return (
+    <div className={cn('flex items-center gap-sm', className)} {...rest}>
+      <Icon name={icon} size={20} />
+
+      <Text.Paragraph className="text-inherit">{children}</Text.Paragraph>
+    </div>
+  );
+}
+
+type FileProps = TFile;
+
+function File(props: FileProps) {
+  const { content, file } = props;
+
+  function onClick() {
+    events.result.show(content);
+  }
+
+  return (
+    <button className="flex w-full mt-xs **:cursor-pointer hover:!text-tone-foreground-context">
+      <Label onClick={onClick} icon="file">
+        {file}
+      </Label>
+    </button>
+  );
+}
+
+type FolderProps = TFolder;
+
+function Folder(props: FolderProps) {
+  const { name, files } = props;
+
+  const hasFiles = !!files.length;
+
+  return hasFiles ? (
+    <div>
+      <Label icon="folder">{name}</Label>
+
+      <div className="ml-md">
+        {files.map(file => (
+          <File key={file.file} {...file} />
+        ))}
+      </div>
+    </div>
+  ) : null;
+}
 
 type TreeProps = {
-  tree: FilesTree[];
+  tree: TFolder[];
 };
 
-const Tree = ({ tree }: TreeProps) => {
+export function Tree(props: TreeProps) {
+  const { tree } = props;
+
   return (
-    <S.Container>
+    <div>
       {tree.map(item => {
         const hasFiles = !!item.files.length;
         const isRoot = !item.name;
@@ -29,8 +89,6 @@ const Tree = ({ tree }: TreeProps) => {
 
         return <Folder key={item.name} {...item} />;
       })}
-    </S.Container>
+    </div>
   );
-};
-
-export { Tree };
+}

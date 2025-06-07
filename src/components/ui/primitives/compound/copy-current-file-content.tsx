16d2c18fcc2294e42222d29c8b4de9bbf68b7821
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-06-07 18:40:44 -0300

    ✨ feat: add copy-current-file-content component

---
+++ src/components/ui/primitives/compound/copy-current-file-content.tsx
@@ -0,0 +1,28 @@
+import React, { JSX, useEffect, useState } from 'react';
+
+import { events } from 'app';
+import { Events } from 'types';
+
+import { CopyChildrenArgs, CopyToClipboard } from './copy-to-clipboard';
+
+type CopyCurrentFileContentProps = {
+  children: (args: CopyChildrenArgs) => JSX.Element;
+};
+
+export function CopyCurrentFileContent(props: CopyCurrentFileContentProps) {
+  const [content, setContent] = useState('');
+
+  function onShowContent(event: CustomEvent<string>) {
+    setContent(event.detail);
+  }
+
+  useEffect(() => {
+    events.on(Events.RESULT_SHOW_CONTENT, onShowContent);
+
+    return () => {
+      events.off(Events.RESULT_SHOW_CONTENT, onShowContent);
+    };
+  }, []);
+
+  return <CopyToClipboard content={content}>{props.children}</CopyToClipboard>;
+}

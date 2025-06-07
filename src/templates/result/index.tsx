f96abcebea55b8ea785d37082c015825d3077775
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-06-07 18:43:22 -0300

    ⚡ chore: add footer to the result page

--- src/templates/result/index.tsx
+++ src/templates/result/index.tsx
@@ -1,6 +1,10 @@
 import { ReadmeResult } from 'components';
+import { Icon } from 'components/ui/primitives/atoms/icon';
 import { Page } from 'components/ui/primitives/atoms/page';
 import { Panel } from 'components/ui/primitives/atoms/panel';
+import { PageFooter } from 'components/ui/common/page-footer';
+import { Clickable } from 'components/ui/primitives/atoms/clickable';
+import { CopyCurrentFileContent } from 'components/ui/primitives/compound/copy-current-file-content';
 
 import { PanelsEnum } from 'types';
 
@@ -16,6 +20,26 @@ const ResultTemplate = () => {
         <Page.Content>
           <ReadmeResult />
         </Page.Content>
+
+        <PageFooter.Container>
+          <PageFooter.Owner />
+          <PageFooter.Navs />
+
+          <CopyCurrentFileContent>
+            {({ copy, isCopied }) => {
+              return (
+                <Clickable.Button
+                  onClick={copy}
+                  tone="success"
+                  className="w-[166px]"
+                >
+                  <Icon name={isCopied ? 'check' : 'copy'} />
+                  {isCopied ? 'Copied 🎉' : 'Copy File Content'}
+                </Clickable.Button>
+              );
+            }}
+          </CopyCurrentFileContent>
+        </PageFooter.Container>
       </Page.Wrapper>
 
       <Panel.Template.Full

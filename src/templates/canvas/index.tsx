9717c6f93ae738dfdf3217da117bf6a29b4b1b2c
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-06-08 18:18:48 -0300

    ⚡ chore: remove unused api call

--- src/templates/canvas/index.tsx
+++ src/templates/canvas/index.tsx
@@ -1,19 +1,12 @@
-import { useEffect } from 'react';
-
 import { Canvas } from 'components';
 import { Page } from 'components/ui/primitives/atoms/page';
 import { Panel } from 'components/ui/primitives/atoms/panel';
 import { PageFooter } from 'components/ui/common/page-footer';
 import { Clickable } from 'components/ui/primitives/atoms/clickable';
 
-import { api } from 'services';
 import { PanelsEnum } from 'types';
 
 const CanvasTemplate = () => {
-  useEffect(() => {
-    api.put('visits');
-  }, []);
-
   return (
     <Page.Container>
       <Panel.Template.Full initialPanel={PanelsEnum.NEW_SECTION} side="left" />

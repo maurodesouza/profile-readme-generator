3a756ccf18fb82966b183d15c9afcfa737590f03
Mauro de Souza <maurodesouza2017@hotmail.com>
2025-09-25 11:48:13 -0300

    ⚡  chore: update recommended resources fields

--- src/components/panels/recommended-resources/items.ts
+++ src/components/panels/recommended-resources/items.ts
@@ -5,9 +5,9 @@ function getItems(locale: string) {
     ...(isBR
       ? [
           {
-            image:
+            imageSrc:
               'https://raw.githubusercontent.com/maurodesouza/maurodesouza/4379dc63fe618ac97c1443b5295861434024e869/assets/typescript-logo.svg',
-            name: 'Boost Your Dev Career',
+            title: 'Boost Your Dev Career',
             description:
               'Master one of the most sought-after technologies in the world!',
             link: 'https://hotm.art/RONICaCT',
@@ -16,8 +16,8 @@ function getItems(locale: string) {
         ]
       : [
           {
-            image: '/assets/books/clean_code_book.jpg',
-            name: 'Clean Code',
+            imageSrc: '/assets/books/clean_code_book.jpg',
+            title: 'Clean Code',
             description:
               'A Handbook of Agile Software Craftsmanship by Uncle Bob.',
             link: 'https://amzn.to/4bR3wGF',
@@ -26,15 +26,15 @@ function getItems(locale: string) {
         ]),
 
     {
-      image: '/assets/books/clean_architecture_book.jpg',
-      name: 'Clean Architecture',
+      imageSrc: '/assets/books/clean_architecture_book.jpg',
+      title: 'Clean Architecture',
       description: 'Software Architecture Solutions from Uncle Bob.',
       link: 'https://amzn.to/41Sg2RG',
       linkLabel: 'Get the book',
     },
     {
-      image: '/assets/books-image.webp',
-      name: 'Explore More Books',
+      imageSrc: '/assets/books-image.webp',
+      title: 'Explore More Books',
       description:
         'Browse the full list of curated titles to boost your coding skills.',
       link: '/resources/books',

f505e3f7e7123e0daebb6ffc340be36ce96616d2
Blueteemo <13031692780@163.com>
2026-05-24 19:44:35 +0800

    fix: add default theme class to html element for SSR compatibility

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
   render(): import('react/jsx-runtime').JSX.Element {
     return (
-      <Html>
+      <Html className="theme-dark">
         <Head />
         <body className="tone palette-brand base-1">
           <Main />

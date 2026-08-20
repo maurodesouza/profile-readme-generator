5eaa385c0d56fe76044897986c3ba3acbcde2e65
Mauro de Souza <maurodesouza.me@gmail.com>
2026-08-19 23:08:36 -0300

    ✨ feat: open templates panel when clicking template preview in welcome screen
    - Add PanelsEnum import to welcome component
    - Show right panel with TEMPLATES view when user clicks a template

--- src/components/molecules/welcome/index.tsx
+++ src/components/molecules/welcome/index.tsx
@@ -9,7 +9,7 @@ import { ShareModal } from '#/components/molecules/share-modal';
 import { DisplayBlock } from '#/components/atoms/display-block';
 
 import { templates } from '#/resources';
-import { CanvasSection } from '#/types';
+import { CanvasSection, PanelsEnum } from '#/types';
 
 const MAX_TEMPLATES_DISPLAY = 8;
 
@@ -41,9 +41,10 @@ export function Welcome() {
             .map(({ template }, index) => (
               <button
                 key={index}
-                onClick={() =>
-                  actions.canvas.preview.sections(template as CanvasSection[])
-                }
+                onClick={() => {
+                  actions.canvas.preview.sections(template as CanvasSection[]);
+                  actions.panel.right.show(PanelsEnum.TEMPLATES);
+                }}
               >
                 <DisplayBlock.Container>
                   <DisplayBlock.Content>
